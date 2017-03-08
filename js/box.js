fc664bcb20273badcbe4868a882a9dee03933c6a
erikjinton <erik.jinton@gmail.com>
2017-03-08 09:04:54 +0100

    Movement
    bruh

---
+++ js/box.js
@@ -0,0 +1,9 @@
+//Objekt för box
+function Box(posX, posY, width, height){
+    
+    //Deklarera egenskaper
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-var ctx;
+var ctx, progressX = 0, progressVX = 0;
 
 //Instansiera spelarobjekt
 var pl = new Player(300, 510, 35, 120);
+
+//Instansiera array med boxar
+var box = [
+    new Box(500, 630 - 50, 50, 50),
+    new Box(500, 630 - 100, 50, 50),
+    new Box(500 - 50, 630 - 50, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50),
+    //new Box(500, 630, 50, 50);
+    
+];
 
 //
 function init() {
@@ -9,7 +29,7 @@ function init() {
     var canvas = document.getElementById("canv");
     ctx = canvas.getContext("2d");
     
-    window.setInterval(update, 20);
+    window.setInterval(update, 20); 
     
     //createjs.Sound.registerSound("ljud/horn.mp3", "mySound");
 }
@@ -18,4 +38,41 @@ function update() {
     
     ctx.clearRect(0, 0, 1200, 630);
     ctx.fillRect(pl.posX, pl.posY, pl.width, pl.height);
+    
+    for (var i = 0; i < box.length; i++) {
+        ctx.fillRect(box[i].posX - progressX, box[i].posY, box[i].width, box[i].height);
+    }
+    
+    progressX = progressX + progressVX;
+}
+
+function keyDown(e) {
+    
+    //Höger
+        if (e.keyCode == 39) {
+            progressVX = progressVX + 10
+        }
+    
+    //Vänster
+            if (e.keyCode == 37) {
+            progressVX = progressVX - 10
+        }
+    
+    //Upp
+            /* if (e.keyCode == 39) {
+            progressVX = progressVX + 10
+        } */
+}
+
+function keyUp(e) {
+    
+    //Släpp höger
+    if (e.keyCode == 39) {
+            progressVX = 0
+        }
+    
+    //Släpp vänster
+    if (e.keyCode == 37) {
+            progressVX = 0
+        }
 }
